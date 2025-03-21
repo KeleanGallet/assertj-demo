25e74854c935e44717670c897ad3bb2e5296ba7b
kelean <kelean.gallet@gmail.com>
2025-03-21 16:04:26 +0100

    feat: add addition tests

---
+++ src/main/java/octogone/examples/Calculator.java
@@ -0,0 +1,7 @@
+package octogone.examples;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/octogone/examples/CalculatorTest.java
@@ -0,0 +1,27 @@
+package octogone.examples;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("Should return the sum of two numbers. (Classic assertion)")
+    void testAddWithClassicAssertion() {
+        var expected = 2;
+        var actual = new Calculator().add(1, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Should return the sum of two numbers. (AssertJ)")
+    void testAddWithAssertJ() {
+        var expected = 2;
+        var actual = new Calculator().add(1, 1);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
